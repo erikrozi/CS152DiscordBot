d9f5f7b9d59db70fbb3ba8165578ee95886977b4
Ashlee <aekupor@gmail.com>
2022-02-11 19:14:52 -0800

    Check if user has too many false reports

--- modFlow.py
+++ modFlow.py
@@ -1,6 +1,24 @@
 # modFlow.py
 from report import Report
 
+user_false_reports = {}
+
 def new_report_filed(completed_report, user_being_reported, user_making_report, reports_by_user, reports_about_user):
-    print("new report received")
+    # Check if abuse or not.
+    is_abuse = True # TODO: figure out if abuse or not
+
+    # TODO: make all print statements into return messages.
+    if not is_abuse:
+        # Add to false reporting map.
+        if user_making_report not in user_false_reports:
+            user_false_reports[user_making_report] = []
+        user_false_reports[user_making_report].append(completed_report)
+
+        # Check if user has > 30 false reports.
+        if len(user_false_reports[user_making_report]) > 30:
+            print("Your account has been banned due to too many false reports.")
+        else:
+            print("We did not find this post to be abusive. Please email us if you think we made a mistake.")
+        return
+
 
