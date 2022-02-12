00e0c5b603ed3f99a46b6c973259dc1ac4c3d6ef
Ashlee <aekupor@gmail.com>
2022-02-11 19:29:04 -0800

    Add flow for spam report

--- modFlow.py
+++ modFlow.py
@@ -3,6 +3,7 @@ from report import Report, ReportType
 
 user_false_reports = {}
 
+
 def new_report_filed(completed_report, user_being_reported, user_making_report, reports_by_user, reports_about_user):
     # Check if abuse or not.
     is_abuse = True # TODO: figure out if abuse or not
@@ -26,7 +27,7 @@ def new_report_filed(completed_report, user_being_reported, user_making_report, 
     if report_type == ReportType.HARASSMENT_BULLYING:
         general_harassment_report(completed_report)
     elif report_type == ReportType.SPAM:
-        spam_report(completed_report)
+        spam_report(reports_about_user[user_being_reported])
     elif report_type == ReportType.HATE_SPEECH:
         general_harassment_report(completed_report)
     elif report_type == ReportType.THREATENING_DANGEROUS:
@@ -36,14 +37,27 @@ def new_report_filed(completed_report, user_being_reported, user_making_report, 
     else: # Other
         general_harassment_report(completed_report)
 
-def spam_report(report):
-    print("Spam")
+
+def spam_report(reports_about_user_list):
+    # Check counts of spam already against user.
+    spam_count = 0
+    for report in reports_about_user_list:
+        if report.report_type is ReportType.SPAM:
+            spam_count += 1
+
+    if spam_count > 30:
+        print("Your account has been banned due to too many spam messages.")
+    else:
+        print("Your post has marked as spam and has been removed. Please email us if you think we made a mistake.")
+
 
 def threatening_dangerous_report(report):
     print("Threatening/Dangerous")
 
+
 def sexual_report(report):
     print("Sexual")
+
 
 def general_harassment_report(report):
     print("General harassment")
